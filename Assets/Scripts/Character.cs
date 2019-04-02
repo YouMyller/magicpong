dbfe7c946e67fda00b389b6430c7f7c0dc17070e
Myllylä Juuso TTK17SP <juusomyllyla1@kamk.fi>
2019-04-02 15:48:20 +0300

    Most of the pingpong basics

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -10,15 +10,22 @@ public class Character : MonoBehaviour
     int def;
     float speed;
 
+    float moveMin;      //Nää vois hakee automattisesti
+    float moveMax;       
+
     public enum Spell { Kakka, Fart };
     public Spell spell;
 
-    public enum Type { Water, Fire };
+    public enum Type { Test, Water, Fire };
     public Type type;
 
     // Start is called before the first frame update
     void Start()
     {
+        if(type == Type.Test)
+        {
+            speed = 1;
+        }
         //Init
         //Call stats from db?
     }
@@ -31,19 +38,22 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Move left/right
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector2.right);
-        }
-        else if(Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector2.left);
-        }
+        //if (transform.position.x >= moveMax || transform.position.x <= moveMin)
+        //{
+            //Move left/right
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate((Vector2.right / 10) * speed);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate((Vector2.left / 10) * speed);
+            }
+        //}
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        //Assign attack power & type to ball
+        //Assign attack power & type to attack
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Attack attack;
-    public Character character;
+    public GameObject attack;
+
+    public GameObject character1;
+    public GameObject character2;
+
+    Vector3 attackStartPos;
+    Quaternion attackRot;
+
+    Vector3 chara1Pos;
+    Quaternion chara1Rot;
+    Vector3 chara2Pos;
+    Quaternion chara2Rot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        chara1Rot = character1.transform.rotation;
+        chara2Rot = character2.transform.rotation;
+        attackRot = attack.transform.rotation;
+
+        Instantiate(character1, chara1Pos, chara1Rot);
+        Instantiate(character2, chara2Pos, chara2Rot);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void StartCombat()
+    {
+
     }
 }
