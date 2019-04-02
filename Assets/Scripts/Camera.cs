8b30e28c75fbafdfd166e53a4e3c9442981d76c2
Myllylä Juuso TTK17SP <juusomyllyla1@kamk.fi>
2019-04-02 14:27:02 +0300

    Some base scripts

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    GameObject myself;
+
+    Vector3 myPos;
+    Vector3 enemyPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Position is behind "myself"
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Keep camera rotated towards enemy
+        Vector3.RotateTowards(myPos, enemyPos, 135, 10);
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    //Stats         //Idea: Statsit vois hakee scriptableista
+    int hp;
+    int atk;
+    int def;
+    float speed;
+
+    public enum Spell { Kakka, Fart };
+    public Spell spell;
+
+    public enum Type { Water, Fire };
+    public Type type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Init
+        //Call stats from db?
+    }
+
+    void Update()
+    {
+        //Use spells
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Move left/right
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Assign attack power & type to ball
+    }
+}
