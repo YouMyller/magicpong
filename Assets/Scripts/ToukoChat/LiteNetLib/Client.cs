9e26d9144c12dc40aea2daaaa8961c29651d66cb
Niininen Touko TTK17SP <toukoniininen@kamk.fi>
2019-04-23 15:55:20 +0300

    Add server and LiteNetLib into project

---
+++ Assets/Scripts/ToukoChat/LiteNetLib/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using LiteNetLib;
+
+public class Client : MonoBehaviour
+{
+    NetManager client;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventBasedNetListener listener = new EventBasedNetListener();
+        client = new NetManager(listener);
+
+        client.Start();
+        client.Connect("localhost" /* host ip or name */, 9050 /* port */, "SomeConnectionKey" /* text key or NetDataWriter */);
+        listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
+        {
+            //Console.WriteLine("We got: {0}", dataReader.GetString(100 /* max length of string */));
+            dataReader.Recycle();
+        };
+
+        /*
+        while (!Console.KeyAvailable)
+        {
+            client.PollEvents();
+            Thread.Sleep(15);
+        }
+        */
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            client.Stop();
+        }
+    }
+}
---
+++ Server/Server/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(9050 /* port */);
+
+            listener.ConnectionRequestEvent += request =>
+            {
+                if (server.PeersCount < 10 /* max connections */)
+                    request.AcceptIfKey("SomeConnectionKey");
+                else
+                    request.Reject();
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                Console.WriteLine("We got connection: {0}", peer.EndPoint); // Show peer ip
+                NetDataWriter writer = new NetDataWriter();                 // Create writer class
+                writer.Put("Hello client!");                                // Put some string
+                peer.Send(writer, DeliveryMethod.ReliableOrdered);             // Send with reliability
+            };
+
+            listener.PeerConnectedEvent -= peer =>
+            {
+                Console.WriteLine("We lost connection.");
+            };
+
+            
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(15);
+            }
+            server.Stop();
+        }
+    }
+}
